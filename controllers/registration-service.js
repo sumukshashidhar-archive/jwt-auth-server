b71222152521662f8ce71f5f066093cd32737aef
sumukshashidhar <sumukuuu@gmail.com>
2020-09-18 16:39:15 +0530

    :bug: Fixed a rename error

--- controllers/registration-service.js
+++ controllers/registration-service.js
@@ -1,14 +1,14 @@
 const user  = require("./../models/user")
 const hashing = require("./auth-microservice")
-const conflict = require("./conflictCheckService")
+const conflict = require("./conflict_check-microservice")
 module.exports = {
     makeUser: async function(username, password, role) {
         return new Promise((resolve, reject) => {
             // then check if the user already exists using a function
-            const isExisting = await conflict.checkUser(username);
+            const isExisting = conflict.checkUser(username);
             if(isExisting) {
                 // if the function returns true, hash the password, and create the user
-                const hashedPassword = await hashing.hashPassword(password);
+                const hashedPassword = hashing.hashPassword(password);
                 const newUser = new user({
                     user:username,
                     password:hashedPassword,
