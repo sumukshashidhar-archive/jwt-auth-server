b2b1302b63148998a60880d6a42d0146c6851b6a
sumukshashidhar <sumukuuu@gmail.com>
2020-09-18 16:22:55 +0530

    :tada: Added a password hashing function

--- controllers/auth-microservice.js
+++ controllers/auth-microservice.js
@@ -1,6 +1,20 @@
 const bcrypt = require("bcrypt")
 module.exports = {
 
+    hashPassword: async function(plaintext_password) {
+        return new Promise((resolve, reject) => {
+            bcrypt.hash(plaintext_password, saltRounds, function(err, hashedPassword) {
+                if(err) {
+                    console.error(err);
+                    reject(err);
+                }
+                else {
+                    console.debug("Hashed a password: ", hashedPassword)
+                    resolve(hashedPassword);
+                }
+            })
+        })
+    },
 
     pass_validate: (plaintext, hashed) => {
         return new Promise((resolve, reject) => {
