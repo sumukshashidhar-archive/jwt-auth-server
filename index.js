88511ef254b519ebc6acfd20397c2d6260cc9f39
sumukshashidhar <sumukuuu@gmail.com>
2020-09-18 16:01:34 +0530

    :tada: Added an index file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+require('dotenv').config()	
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+require('./routes')(app);
+
+app.listen(process.env.PORT||5000, process.env.IP || "0.0.0.0", function (req, res) {
+	console.info("Server Started")
+})
+
+mongoose.connect(process.env.mongoURI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true})
+    .then(() => console.info("MongoDB Connected"))
+  .catch(err => console.error(err));
