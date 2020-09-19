835623a5a111abe8b4d1ea14583908d5c69263cd
sumukshashidhar <sumukuuu@gmail.com>
2020-09-19 19:04:54 +0530

    :construction_worker: Changed the deployment script to use .env file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-git stash; 
-git pull; 
 docker build . -t sumukshashidhar/jwt-auth-api; 
-docker run  -e mongoURI="mongodb+srv://<username>:<pass>@cluster0.veayr.mongodb.net/<dbname>?retryWrites=true&w=majority" -it -p 3000:3000 sumukshashidhar/jwt-auth-api;
+docker run  --env-file .env -it -p 3000:3000 sumukshashidhar/jwt-auth-api;
