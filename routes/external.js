796d69bdfe21754e0c1987d4341af863088444c2
sumukshashidhar <sumukuuu@gmail.com>
2020-09-19 18:39:39 +0530

    :rocket: Finished the external auth route

--- routes/external.js
+++ routes/external.js
@@ -1,16 +1,15 @@
 const { response } = require('express')
 const jw = require('./../controllers/jwt-microservice')
 module.exports = (app) => {
-    app.post('/api/authenticate/', (req, res)=> {
+    app.post('/api/authenticate/', async (req, res)=> {
         // first check if the token exists  
         if (req.body.token !== null && req.body.token !== undefined && req.body.token !== '') {
             // there is a token supplies, pass it to jwtms
-            var response = jw.verification(req.body.token)
-            console.log(response)
+            var response = await jw.verification(req.body.token)
             if (response !== false) {
                 // now we have a valid, decoded response, we can return it to the user
                 res.json( {
-                    "decodedToken":res
+                    "decodedToken":response
                 })
             }
         }
